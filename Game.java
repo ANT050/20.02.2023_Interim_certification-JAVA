3f331e3160c18d314bdc586b21fc2421072dee86
Anton <tepa-86@mail.ru>
2023-02-21 02:45:02 +0300

    Изменение в файле Game.java:
    - Добавлен счетчик при игре - при выпадании игрушки
      количество этой игрушки в файле toys.csv уменьшается,
      как только количество становится ноль, игрушка удаляется,
      как только игрушек не осталось, то выводится сообщение, что
      файл пустой
    
    - Добавлен счетчик в файл played.csv - при выпадании игрушки,
      если игрушки нет, то добавляется, если такая игрушка есть, то
      увеличивается количество этой игрушки.

--- Game.java
+++ Game.java
@@ -8,7 +8,7 @@ import java.util.Random;
 
 public class Game {
     private static final String TOY_LIST = "toys.csv";
-    private static final String PLAYED_FILE_PATH = "played.csv";
+    private static final String PARTICIPATING_TOYS = "played.csv";
 
     public static void start() {
         List<String> toys = readToysFromFile();
@@ -25,18 +25,22 @@ public class Game {
     }
 
     private static List<String> readToysFromFile() {
-        List<String> toys = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(TOY_LIST))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                toys.add(line);
-            }
-        } catch (IOException e) {
-            System.out.println("\n" + String.format("%s", "\u001B[31m-\u001B[0m".repeat(98)));
-            System.out.println("\u001B[31mОшибка при чтении файла " + TOY_LIST + ": " + e.getMessage() + "\u001B[0m");
-            System.out.println(String.format("%s", "\u001B[31m-\u001B[0m".repeat(98)));
-        }
-        return toys;
+      List<String> toys = new ArrayList<>();
+      try (BufferedReader reader = new BufferedReader(new FileReader(TOY_LIST))) {
+          String line;
+          while ((line = reader.readLine()) != null) {
+              String[] toyData = line.split(",");
+              int quantity = Integer.parseInt(toyData[2]);
+              if (quantity > 0) {
+                  toys.add(line);
+              }
+          }
+      } catch (IOException e) {
+          System.out.println("\n" + String.format("%s", "\u001B[31m-\u001B[0m".repeat(98)));
+          System.out.println("\u001B[31mОшибка при чтении файла " + TOY_LIST + ": " + e.getMessage() + "\u001B[0m");
+          System.out.println(String.format("%s", "\u001B[31m-\u001B[0m".repeat(98)));
+      }
+      return toys;
     }
 
     private static String selectRandomToy(List<String> toys) {
@@ -58,21 +62,51 @@ public class Game {
     }
 
     private static void writePlayedToyToFile(String toy) {
-      try (BufferedReader reader = new BufferedReader(new FileReader(PLAYED_FILE_PATH))) {
+      List<String> toys = readToysFromFile();
+      boolean toyFound = false;
+      for (int i = 0; i < toys.size(); i++) {
+          String[] toyData = toys.get(i).split(",");
+          if (toyData[1].equals(toy.split(",")[0])) {
+              int quantity = Integer.parseInt(toyData[2]) - 1;
+              toys.set(i, toyData[0] + "," + toyData[1] + "," + quantity + "," + toyData[3]);
+              toyFound = true;
+              break;
+          }
+      }
+
+      if (!toyFound) {
+          System.out.println("\n" + String.format("%s", "\u001B[31m-\u001B[0m".repeat(98)));
+          System.out.println("\u001B[31mОшибка: выбранная игрушка не найдена в файле " + TOY_LIST + "\u001B[0m");
+          System.out.println(String.format("%s", "\u001B[31m-\u001B[0m".repeat(98)));
+          return;
+      }
+
+      try (FileWriter writer = new FileWriter(TOY_LIST)) {
+          for (String toyData : toys) {
+              writer.write(toyData + "\n");
+          }
+      } catch (IOException e) {
+          System.out.println("\n" + String.format("%s", "\u001B[31m-\u001B[0m".repeat(98)));
+          System.out.println("\u001B[31mОшибка при записи файла " + TOY_LIST + ": " + e.getMessage() + "\u001B[0m");
+          System.out.println(String.format("%s", "\u001B[31m-\u001B[0m".repeat(98)));
+          return;
+      }
+
+      try (BufferedReader reader = new BufferedReader(new FileReader(PARTICIPATING_TOYS))) {
           List<String> playedToys = new ArrayList<>();
           String line;
-          boolean toyFound = false;
+          boolean toyFound2 = false;
           while ((line = reader.readLine()) != null) {
               String[] playedToyData = line.split(",");
               if (playedToyData[1].equals(toy.split(",")[0])) {
                   int quantity = Integer.parseInt(playedToyData[2]) + 1;
                   line = playedToyData[0] + "," + playedToyData[1] + "," + quantity;
-                  toyFound = true;
+                  toyFound2 = true;
               }
               playedToys.add(line);
           }
 
-          if (!toyFound) {
+          if (!toyFound2) {
               int id = 0;
               for (String playedToy : playedToys) {
                   String[] playedToyData = playedToy.split(",");
@@ -84,21 +118,22 @@ public class Game {
               playedToys.add(playedToyEntry);
           }
 
-          try (FileWriter writer = new FileWriter(PLAYED_FILE_PATH)) {
+          try (FileWriter writer = new FileWriter(PARTICIPATING_TOYS)) {
               for (String playedToy : playedToys) {
                   writer.write(playedToy + "\n");
               }
-              System.out.println("\nВыбранная игрушка записана в файл: " + PLAYED_FILE_PATH);
+              System.out.println("Выбранная игрушка записана в файл: " + PARTICIPATING_TOYS);
               System.out.println(String.format("%s", "-".repeat(98)));
           } catch (IOException e) {
               System.out.println("\n" + String.format("%s", "\u001B[31m-\u001B[0m".repeat(98)));
-              System.out.println("\u001B[31mОшибка при записи файла " + PLAYED_FILE_PATH + ": " + e.getMessage() + "\u001B[0m");
+              System.out.println("\u001B[31mОшибка при записи файла " + PARTICIPATING_TOYS + ": " + e.getMessage() + "\u001B[0m");
               System.out.println(String.format("%s", "\u001B[31m-\u001B[0m".repeat(98)));
+              return;
           }
       } catch (IOException e) {
           System.out.println("\n" + String.format("%s", "\u001B[31m-\u001B[0m".repeat(98)));
-          System.out.println("\u001B[31mОшибка при чтении файла " + PLAYED_FILE_PATH + ": " + e.getMessage() + "\u001B[0m");
+          System.out.println("\u001B[31mОшибка при чтении файла " + PARTICIPATING_TOYS + ": " + e.getMessage() + "\u001B[0m");
           System.out.println(String.format("%s", "\u001B[31m-\u001B[0m".repeat(98)));
       }
-  }
+    }
 }
