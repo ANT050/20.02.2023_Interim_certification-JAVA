d53a6a3247e8a689749de45e4c1a35b494c34670
Anton <tepa-86@mail.ru>
2023-02-21 06:18:55 +0300

    Добавление изменений

--- OutputTablesToys.java
+++ OutputTablesToys.java
@@ -6,45 +6,34 @@ import java.nio.charset.StandardCharsets;
 public class OutputTablesToys {
     private static final String TOY_LIST = "toys.csv";
     private static final String PARTICIPATING_TOYS = "played.csv";
-
-
-    private static void printToyList(BufferedReader br, boolean printHeader) throws IOException {
-        String line;
-        if (printHeader) {
-            System.out.println(String.format("%s", "-".repeat(98)));
-            System.out.println(String.format("| \u001B[31m%-5s\u001B[0m | \u001B[31m%-30s\u001B[0m | \u001B[31m%-30s\u001B[0m | \u001B[31m%-20s\u001B[0m |", "ID", "Название игрушки", "Количество игрушек", "Процент выпадания"));
-
-            System.out.println(String.format("%s", "-".repeat(98)));
-        }
-
-        while ((line = br.readLine()) != null) {
-            String[] toyData = line.split(",");
-            System.out.println(String.format("| %-5s | %-30s | %-30s | %-20s |", toyData[0], toyData[1], toyData[2], toyData[3]));
-            System.out.println(String.format("%s", "-".repeat(98)));
-        }
-    }
-
+	
     public static void displayToyList() {
         try (BufferedReader br = new BufferedReader(new FileReader(TOY_LIST, StandardCharsets.UTF_8))) {
-            String spaces = new String(new char[22]).replace('\0', ' ');
-            System.out.println("\n" + spaces + "\u001B[33m==== Список всех игрушек участвующих в розыгрыше ====\n\u001B[0m");
-            printToyList(br, true);
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] toyData = line.split(",");
+                System.out.println(toyData[0] + ". " + toyData[1] + ": " + toyData[2] + "шт. " + toyData[3]);
+                System.out.println(String.format("%s", "-".repeat(98)));
+            }
         } catch (IOException e) {
             System.out.println("\n" + String.format("%s", "\u001B[31m-\u001B[0m".repeat(98)));
-            System.out.println("\u001B[31mОшибка чтения файла: " + e.getMessage() + "\u001B[0m");
+            System.out.println("Ошибка чтения файла: " + e.getMessage());
             System.out.println(String.format("%s", "\u001B[31m-\u001B[0m".repeat(98)));
         }
     }
 
     public static void displayParticipatingToys() {
         try (BufferedReader br = new BufferedReader(new FileReader(PARTICIPATING_TOYS, StandardCharsets.UTF_8))) {
-            String spaces = new String(new char[31]).replace('\0', ' ');
-            System.out.println("\n" + spaces + "\u001B[33m==== Список разыгранных игрушек ====\n\u001B[0m");
-            printToyList(br, true);
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] toyData = line.split(",");
+                System.out.println(toyData[0] + ". " + toyData[1] + ": " + toyData[2] + "шт. ");
+                System.out.println(String.format("%s", "-".repeat(98)));
+            }
         } catch (IOException e) {
             System.out.println("\n" + String.format("%s", "\u001B[31m-\u001B[0m".repeat(98)));
-            System.out.println("\u001B[31mОшибка чтения файла: " + e.getMessage() + "\u001B[0m");
+            System.out.println("Ошибка чтения файла: " + e.getMessage());
             System.out.println(String.format("%s", "\u001B[31m-\u001B[0m".repeat(98)));
         }
-    }
+    }   
 }
